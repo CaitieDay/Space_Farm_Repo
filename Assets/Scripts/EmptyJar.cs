b2311fd649aec990883298312f44bb8ee30d64ad
Caitie Day <catherineday608@gmail.com>
2021-06-07 17:04:14 +1000

    Started a script
    it has one line, barely functioning

---
+++ Assets/Scripts/EmptyJar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyJar : MonoBehaviour
+{
+    // I need jarEmpty and jarFull to be referenced to something but i don;t know what yet.
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 
+    }
+}
